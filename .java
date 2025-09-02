439392a24bdce03b898d3bafceaaa3c081bd86a0
kharshita590 <83570759+kharshita590@users.noreply.github.com>
2025-09-02 12:16:44 +0530

    Create .java

---
+++ .java
@@ -0,0 +1,21 @@
+package exception;
+
+public class room {
+	public static void main[(String[] args){
+		try {
+		int a=10;
+		int b=0;
+		int c=a/b;
+		int arr[]=new int[5];
+		System.out.println(arr[6]);
+		System.out.println(c);
+		}catch(ArithemticException e) {
+			System.out.println("Error");
+		}
+		finally {
+			System.out.println("End of program");
+		}
+	}
+
+}
+
